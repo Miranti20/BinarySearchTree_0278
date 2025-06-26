661080050365ab97851af10fbe839b04e34137fa
Miranti20 <rantirarrr4505@gmail.com>
2025-06-26 10:17:32 +0700

    Membuat class Node

---
+++ Binary Search Tree.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Node
+{
+public:
+    int info;
+    Node* leftchild;
+    Node* rightchild;
+
+    // construktor for the node class
+    Node()
+    {
+        leftchild = nullptr; // intialieze left hild to null
+        rightchild = nullptr; // intialieze right child to null
+    }
+};
+
+
+
+
